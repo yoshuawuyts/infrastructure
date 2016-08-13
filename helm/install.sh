7b853dc1abe6b90f1991247d97a7b6546554e247
Yoshua Wuyts <yoshuawuyts@gmail.com>
2016-08-13 16:55:51 +0200

    kube: add buildkite

--- helm/install.sh
+++ helm/install.sh
@@ -1,8 +1,25 @@
 #!/bin/sh
+# from https://github.com/buildkite/helm-charts
 
-set -e
+if ! [ -d '../secrets' ]; then
+  printf 'cannot find dir ../secrets'
+  exit 1
+fi
 
-printf '[helm] initializing helm-charts'
+printf '[helm] initializing helm-charts\n'
 helmc up
 helmc repo add buildkite https://github.com/buildkite/helm-charts
-helmc up
+[ $? -eq 0 ] && helmc up
+
+printf '[helm] fetching buildkite image\n'
+helmc fetch buildkite/buildkite-agent-builder-private-git
+
+printf '[helm] copying secrets\n'
+cp ../secrets/helm/buildkite.toml \
+  "$(helmc home)"/workspace/charts/buildkite-agent-builder-private-git/tpl/settings.toml
+
+printf '[helm] creating local files\n'
+helmc generate -f buildkite-agent-builder-private-git
+
+printf '[helm] installing on kubernetes\n'
+helmc install buildkite-agent-builder-private-git
